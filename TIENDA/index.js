e3d134486352956f83871a9911e739f71e117d85
Sebastian Nuñez <sebas11np@gmail.com>
2023-03-31 14:31:26 -0600

    Articulos extraidos

--- TIENDA/index.js
+++ TIENDA/index.js
@@ -1,20 +1,24 @@
 console.log('Happy hacking :)')
 
-const API = "https://platzi-avo.vercel.app/api/avo";
+const urlBase = "https://platzi-avo.vercel.app";
+const containerNode = document.querySelector("#container");
 
-fetch(API)
+fetch(`${urlBase}/api/avo`)
 .then((response) => response.json())
 .then((responseJson) => {
-    responseJson.data.forEach(items => {
+    responseJson.data.forEach(item => {
         //crear imagen
         const imagen = document.createElement("img");
+        imagen.src = `${urlBase}${item.image}`;
         //crear titulo
         const titulo = document.createElement("h2");
+        titulo.textContent = item.name;
         // crear precio
         const precio = document.createElement("h3");
+        precio.textContent = item.price;
         //contenedor
         const contenedor = document.createElement("div");
         contenedor.append(imagen, titulo, precio);
-        document.body.append(contenedor);
+        containerNode.append(contenedor);
     });
 })
